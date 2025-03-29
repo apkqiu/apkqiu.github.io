6117b7b886302b0aeb56e3e26be4f54f550f910f
爱皮卡丘 <apkqiu@qq.com>
2025-03-29 21:55:42 +0800

    重构 do.py，添加并行下载和上传功能，改进 git pull 和 git push 的成功率统计

--- do.py
+++ do.py
@@ -1,7 +1,19 @@
-import os
-i = 0
-while True:
-    i+=1
-    print(f"#{i}")
-    os.system("git pull --verbose")
-    print()
+import os, multiprocessing
+def download():
+    i = 0
+    succ = 0
+    while True:
+        i+=1
+        print(f"Download #{i} ({succ*100/i:.0f}% success)")
+        succ += os.system("git pull --verbose > NUL 2>&1")
+def upload():
+    i = 0
+    succ = 0
+    while True:
+        i+=1
+        print(f"Upload #{i} ({succ*100/i:.0f}% success)")
+        succ += not os.system("git push --verbose > NUL 2>&1")
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    multiprocessing.Process(target=upload).start()
+    multiprocessing.Process(target=download).start()
