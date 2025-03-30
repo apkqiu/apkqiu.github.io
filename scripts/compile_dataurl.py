ef4546800c4c5bad29bc2e576d300fa6b0046fe5
爱皮卡丘 <apkqiu@qq.com>
2025-03-30 16:05:35 +0800

    修复路径处理错误，确保正确创建src目录

--- scripts/compile_dataurl.py
+++ scripts/compile_dataurl.py
@@ -14,7 +14,7 @@ for dirpath, dirnames, filenames in os.walk("docs"):
             f = open(os.path.join(dirpath, filename), "r", encoding="utf-8")
             lines = f.read()
             f.close()
-            os.makedirs("src/"+dirpath.removeprefix("docs/"),exist_ok=True)
+            os.makedirs("src/"+dirpath.removeprefix("docs"),exist_ok=True)
             f = open(os.path.join("src/"+dirpath.removeprefix("docs/"),
                      filename), "w", encoding="utf-8")
             f.write(regex.sub(lambda match: f'![{match.group(0)}]({image_to_dataurl(match.group(1))})', lines))
